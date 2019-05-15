c7ce809df167960b505b78d2bce902499d25e0d7
Andrew Dillon <majik895@gmail.com>
2019-05-15 15:56:07 -0400

    HTML Corrections

--- Article/Article.js
+++ Article/Article.js
@@ -4,18 +4,29 @@ class Article {
     constructor(domElement) {
         // assign this.domElement to the passed in domElement
         this.domElement = domElement;
+        console.log(this.domElement);
         // create a reference to the ".expandButton" class. 
-        this.expandButton = this.domElement.querySelector(".expandButton");
+        this.expandButton = this.domElement.querySelectorAll('.expandButton');
         // Using your expandButton reference, update the text on your expandButton to say "expand"
-        this.expandButton.innerHTML = "expand";
+        this.expandButton.forEach((button) => {
+            button.textContent = 'expand';
+        })
+
         // Set a click handler on the expandButton reference, calling the expandArticle method.
-        this.expandButton.addEventListener("click", () => this.expandArticle());
+        this.expandButton.forEach((button) => {
+            button.addEventListener('click', (e) => {
+                this.expandArticle(e);
+            })
+        })
+
+
     }
 
     expandArticle() {
         // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+        console.log('test worked');
         this.domElement.classList.toggle('article-open');
+
     }
 }
 
@@ -25,9 +36,8 @@ class Article {
 */
 
 let articles = document.querySelectorAll('.article');
+console.log(articles);
 
-articles.forEach(article => {
+articles.forEach((article) => {
     return new Article(article);
-});
-
-// console.log(articles);
+})
